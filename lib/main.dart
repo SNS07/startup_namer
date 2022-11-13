ecf40a97c0c4f599b096449a55982167143ad9b5
Sreenath S <sreenaths.s9@gmail.com>
2022-11-13 09:32:06 -0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:startup_namer/Screens/Welcome/welcome_screen.dart';
+import 'package:startup_namer/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Take Me',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
